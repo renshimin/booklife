803a30ce27d176943aba3a0b772364b244a2c31d
renshimin <struggle_yo@163.com>
2017-06-07 17:59:29 +0800

    commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,22 @@
 module.exports = {
     'GET /': async (ctx, next) => {
         ctx.render('index.html', {
-            title: '你好'
+            title: '首页'
+        });
+    },
+    'GET /login': async (ctx, next) => {
+        ctx.render('login.html', {
+            title: '登录'
+        });
+    },
+    'GET /register': async (ctx, next) => {
+        ctx.render('register.html', {
+            title: '注册'
+        });
+    },
+    'GET /edit': async (ctx, next) => {
+        ctx.render('edit.html', {
+            title: '编辑个人资料'
         });
     }
 };
